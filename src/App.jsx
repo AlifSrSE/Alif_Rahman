f47df6588458d81e41802f5d763bcef6d31bd1d5
ALIF RAHMAN <alif.rahman.c@gmail.com>
2025-07-01 09:52:33 +0600

    Update App.jsx
    Loader name changes

--- src/App.jsx
+++ src/App.jsx
@@ -230,7 +230,7 @@ function App() {
   if (!isAuthReady) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
-        <p className="text-xl animate-pulse">Loading your personal website...</p>
+        <p className="text-xl animate-pulse">Loading Alif Rahman</p>
       </div>
     );
   }
